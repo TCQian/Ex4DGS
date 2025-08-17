689071d9c4f85ee49f98dec8a1ee855a25d4292e
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2025-08-17 21:24:47 +0800

    Update soc-cluster.sh

--- soc-cluster.sh
+++ soc-cluster.sh
@@ -10,5 +10,8 @@
 source ~/.bashrc
 conda activate Ex4DGS
 
-python train.py --config configs/N3V/n3v_base.json --model_path output/dynerf/cut_roasted_beef  --source_path data/dynerf/cut_roasted_beef
-python render.py --model_path output/dynerf/cut_roasted_beef --source_path data/dynerf/cut_roasted_beef --skip_train --iteration 40000
+# python train.py --config configs/N3V/n3v_base.json --model_path output/dynerf/cut_roasted_beef  --source_path data/dynerf/cut_roasted_beef
+# python render.py --model_path output/dynerf/cut_roasted_beef --source_path data/dynerf/cut_roasted_beef --skip_train --iteration 40000
+
+python train.py --config configs/N3V/n3v_base.json --model_path output/cmu/basketball  --source_path data/cmu/basketball
+python render.py --model_path output/cmu/basketball --source_path data/cmu/basketball --skip_train --iteration 40000
